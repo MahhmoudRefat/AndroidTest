124d20038663217786ba21463e2c37bb003e5860
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-06-04 16:53:02 +0300

    new form with PERMISSIONS  test 1

--- app/src/main/java/com/cobra/philipfirsttest/MainActivity.kt
+++ app/src/main/java/com/cobra/philipfirsttest/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.cobra.philipfirsttest
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
 import java.util.*
 
 //import kotlinx.android.synthetic.main.activity_main.*
@@ -13,13 +16,64 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        var button = findViewById<Button>(R.id.btnrequest)
+    button.setOnClickListener {
 
     }
 
+    }
 
+    private fun haswriteexternal() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun location() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun loctionbackground() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestpermission() {
+        var Permissiontorequest = mutableListOf<String>()
+        if (!haswriteexternal()) {
+            Permissiontorequest.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        if (!location()) {
+            Permissiontorequest.add(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+        if (!loctionbackground()) {
+            Permissiontorequest.add(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+        if (Permissiontorequest.isNotEmpty()){
+            ActivityCompat.requestPermissions(this,Permissiontorequest.toTypedArray(),0)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    if(requestCode == 0 &&  grantResults.isNotEmpty()){
+        for (i in grantResults.indices ){
+            if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                Log.d("permissionRequest","${permissions[i] } granted.")
+            }
+        }
+    }
+    }
 }
+
+
 /* var btnshowtoast = findViewById<Button>(R.id.btnShowToast)
        btnshowtoast.setOnClickListener {
             Intent(this, SecondActivity::class.java).also {
